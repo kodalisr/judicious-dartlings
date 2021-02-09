2a1a26bdf4e03662543d15fc5c7f9c0724685884
Sree Kodali <kodalisr@vcu.edu>
2021-02-09 18:36:46 -0500

    new files

---
+++ RebOriginalCode.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 19:08:02 2021
+
+@author: RebekahDSK
+"""
+
+import requests
+import pandas as pd
+
+url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
+
+querystring = {"limitLicense": "<REQUIRED>",
+	"offset": "0",
+	"number": "10",
+	"query": "burger",
+	"cuisine": "american",
+    "addRecipeInformation": "True",
+	"includeIngredients": "onions, lettuce, tomato",
+	"excludeIngredients": "coconut, mango",
+	"intolerances": "peanut, shellfish",
+	"type": "main course",
+	"ranking": "2",
+	"minCalories": "150",
+	"maxCalories": "1500",
+	"minFat": "5",
+	"maxFat": "100",
+	"minProtein": "5",
+	"maxProtein": "100",
+	"minCarbs": "5",
+	"maxCarbs": "100",} ### CHANGE INGREDIENT TO WHAT YOU WANT
+
+headers = {
+    'x-rapidapi-key': "9e12485098mshdefbf3ff62ef150p1717ddjsn1cf8f48a5741",
+    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+
+list1 = response.json()
+
+df =pd.DataFrame(list1)
+
+#unpack columns in dataframe
+
+def unpack(df, column, fillna=None):
+    ret = None
+    if fillna is None:
+        ret = pd.concat([df, pd.DataFrame((d for idx, d in df[column].iteritems()))], axis=1)
+        del ret[column]
+    else:
+        ret = pd.concat([df, pd.DataFrame((d for idx, d in df[column].iteritems())).fillna(fillna)], axis=1)
+        del ret[column]
+    return ret
+
+df1 = unpack(df, 'results', 0)
+df2=  unpack(df1, 0, 0)
---
+++ WhatRebThinksWillWork.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import requests
+import pandas as pd
+from flask import Flask, request, render_template, jsonify
+
+# %%
+
+def queryRecipeEndpoint(query, cuisine):
+
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex"
+    
+    # these will come from form controls
+    query = query
+    cuisine = cuisine
+    type_of_recipe = 'main course'
+    ranking = "2"
+    minCalories = "150"
+    maxCalories = "1500"
+    minFat = "5"
+    maxFat = "100"
+    minProtein = "5"
+    maxProtein = "100"
+    minCarbs = "5"
+    maxCarbs = "100"
+    
+    querystring = {"limitLicense": "<REQUIRED>",
+        "offset": "0",
+        "number": "10",
+        "query": query,
+        "cuisine": cuisine,
+        #"includeIngredients": "onions, lettuce, tomato",
+        #"excludeIngredients": "coconut, mango",
+        #"intolerances": "peanut, shellfish",
+        "type": type_of_recipe,
+        "ranking": ranking,
+        "minCalories": minCalories,
+        "maxCalories": maxCalories,
+        "minFat": minFat,
+        "maxFat": maxFat,
+        "minProtein": minProtein,
+        "maxProtein": maxProtein,
+        "minCarbs": minCarbs,
+        "maxCarbs": maxCarbs,
+        "instructionsRequired": "True",
+        "addRecipeInformation": "True",
+        "fillIngredients": "True",
+    };
+
+    headers = {
+        'x-rapidapi-key': "9e12485098mshdefbf3ff62ef150p1717ddjsn1cf8f48a5741",
+        'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        }
+    
+    response = requests.get(url, headers=headers, params=querystring)
+    
+    response_json = response.json()
+    
+    results = response_json['results']
+    
+    recipe_ingredients = []
+    
+    
+    # ingredients stuff
+    for result in results:
+        recipe_id = result['id']
+        recipe_title = result['title']
+        cooking_minutes = result['cookingMinutes']
+        health_score = result['healthScore']
+        source_url = result['sourceUrl']
+        likes = result['likes']
+            
+        analyzedInstructions = result['analyzedInstructions']
+        
+        for instruction in analyzedInstructions:
+            
+            steps = instruction['steps']
+            
+            for step in steps:
+                
+                ingredients = step['ingredients']
+                
+                for ingredient in ingredients:
+                    
+                    ingredient_name = ingredient['name']
+                    
+                    recipe_ingredient = {
+                        'recipe_id': recipe_id,
+                        'recipe_title': recipe_title,
+                        'sourceUrl': source_url,
+                        'cooking_minutes': cooking_minutes,
+                        'health_score': health_score,
+                        'likes': likes,
+                        'ingredient_name': ingredient_name
+                    }
+                    
+                    # check to see if the ingredient is already attached a recipe
+                    # if not, append it... if so, dont
+                    recipe_ingredients.append(recipe_ingredient)
+                    
+    recipe_df = pd.DataFrame(recipe_ingredients)
+    recipe_df.drop_duplicates(inplace=True)
+    
+    return recipe_df
+
+# %%
+
+
+
+# %%
+# Flask shit
+
+app = Flask(__name__)
+
+@app.route('/api/ingredients')
+def ingredients():
+    
+    query = request.args.get('query')
+    cuisine = request.args.get('cuisine')
+    
+    recipe_df = queryRecipeEndpoint(query, cuisine)
+    
+    recipe_json = recipe_df.to_json(orient='records')
+    
+    return recipe_json
+
+# %%
+    
+if __name__ == '__main__':
+    app.run(debug=True)
