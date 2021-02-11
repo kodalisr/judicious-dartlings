bdf64f28159f54b0bf5cd980617ab55178ebea63
Katherine Kane <kakane@umich.edu>
2021-02-10 20:37:11 -0500

    adding GoodToGoDONTtouch_partdeux

---
+++ config.py
@@ -0,0 +1,5 @@
+remote_db_endpoint = 'databootcamp.cv1u8ipt6nbb.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'postgres123'
+remote_db_name = 'recipes'
