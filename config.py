62bfefeb9626b9f4a1ac2401b3a6c08d4e6ea0f6
Sree Kodali <kodalisr@vcu.edu>
2021-02-10 21:38:48 -0500

    deleting config

--- config.py
+++
@@ -1,5 +0,0 @@
-remote_db_endpoint = 'databootcamp.cv1u8ipt6nbb.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_user = 'admin'
-remote_db_pwd = 'postgres123'
-remote_db_name = 'recipes'
